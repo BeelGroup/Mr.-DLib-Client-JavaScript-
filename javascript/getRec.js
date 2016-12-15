62c330f0ad9091f4eff745fbdf8482d49364b0e9
Sara Yousif <leen_w2@hotmail.com>
2016-12-15 18:45:18 +0400

    Update getRec.js

--- javascript/getRec.js
+++ javascript/getRec.js
@@ -3,7 +3,7 @@
 $( document ).ready(function() {
 
   var  id = document.getElementById("gesis_id").innerHTML; 
-    var site = "https://gesis.herokuapp.com/gesis2/" + id;
+    var site = "https://GesisHost/proxy.php?id=" + id;
 	var section = document.getElementById("articles");
     $( section ).load(site);
     } );   
