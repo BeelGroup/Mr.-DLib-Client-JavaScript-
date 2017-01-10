b770fc7f8e604a6ad8328c928dd73b001274499e
Sara Yousif <leen_w2@hotmail.com>
2017-01-10 23:25:27 +0400

    Update getRec.js

--- javascript/getRec.js
+++ javascript/getRec.js
@@ -1,9 +1,22 @@
  
 
 $( document ).ready(function() {
+var img = document.createElement("img");
+img.src = "img/loading.gif";
+var loading =document.getElementById("loading");
+loading.appendChild(img);
 
   var  id = document.getElementById("gesis_id").innerHTML; 
-    var site = "https://GesisHost/proxy.php?id=" + id;
-	var section = document.getElementById("articles");
+   var site = "https://GesisHost/proxy.php?id=" + id;
+var section = document.getElementById("articles");
     $( section ).load(site);
     } );   
+
+
+function makeDliblog(aurl,isdlib)
+{
+    if(isdlib=='1'){
+        var img = new Image();
+        img.src = aurl;
+    }
+}
