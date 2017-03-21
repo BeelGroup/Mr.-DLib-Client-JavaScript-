5b8a3c4973959e431807b7a06dad3e0c03444472
Sara Yousif <leen_w2@hotmail.com>
2017-03-21 12:08:27 +0400

    Update getRec.js

--- javascript/getRec.js
+++ javascript/getRec.js
@@ -9,7 +9,10 @@ loading.appendChild(br);
 loading.appendChild(img);
 
 var  id = document.getElementById("gesis_id").innerHTML; 
-var site = "http://sowiport.gesis.org/Dlib/Proxy?id=" + id;
+var  title = document.getElementById("gesis_title").innerHTML; 
+var encodedtitle = encodeURI(title);
+var etitle = encodedtitle.replace(/'/g, "`");
+var site = "http://sowiport.gesis.org/Dlib/Proxy?id=" + id +"&title=" + etitle;
 var section = document.getElementById("articles");
     $( section ).load(site);
     } );   
