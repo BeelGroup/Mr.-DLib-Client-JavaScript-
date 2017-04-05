822f25757f1fffba21386fdd05fcf6dffd636940
Carevic, Zeljko <zeljko.carevic@gesis.org>
2017-04-05 15:40:40 +0200

    added javascript and proxy.php

---
+++ javascript/dlib.js
@@ -0,0 +1,18 @@
+$( window ).ready(function () {
+    var img = document.createElement("img");
+    img.src = "../../../interface/themes/default_gesis/images/icons/loading.gif";
+    var loading = document.getElementById("loading");
+    loading.appendChild(img);
+    var id = document.getElementById("gesis_id").innerHTML;
+    var site = "/Dlib/Proxy?id=" + id;
+    var section = document.getElementById("articles");
+    //$(section).load(site);
+	$.ajax({
+		  type: "GET",
+		  url: site,
+		  async: true,
+		  timeout: 3000, // sets timeout to 3 seconds;
+		  }).success(function(data) {
+			$( section).html( data);
+	})
+}); 
