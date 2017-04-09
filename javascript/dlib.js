302f7ce5db523923691269ebdd8ecc7b88c31e90
Sara Yousif <leen_w2@hotmail.com>
2017-04-10 00:04:39 +0400

    Update dlib.js
    Modify the JavaScript to request Mr. DLib with article ID and title

--- javascript/dlib.js
+++ javascript/dlib.js
@@ -1,11 +1,18 @@
 $( window ).ready(function () {
-    var img = document.createElement("img");
-    img.src = "../../../interface/themes/default_gesis/images/icons/loading.gif";
-    var loading = document.getElementById("loading");
-    loading.appendChild(img);
-    var id = document.getElementById("gesis_id").innerHTML;
-    var site = "/Dlib/Proxy?id=" + id;
-    var section = document.getElementById("articles");
+   
+}); 
+
+function get_rec(){
+	 var img = document.createElement("img");
+    	img.src = "../../../interface/themes/default_gesis/images/icons/loading.gif";
+   	var loading = document.getElementById("loading");
+  	loading.appendChild(img);
+ 	var id = document.getElementById("gesis_id").innerHTML;
+	var  doc_title = document.getElementById("doc_title_full").innerHTML;
+	var encodedtitle = encodeURI(doc_title);
+	var title = encodedtitle.replace(/'/g, "`");
+    	var site = "/Dlib/Proxy?id=" + id +"&title=" + title;
+    	var section = document.getElementById("articles");
     //$(section).load(site);
 	$.ajax({
 		  type: "GET",
@@ -14,5 +21,7 @@ $( window ).ready(function () {
 		  timeout: 3000, // sets timeout to 3 seconds;
 		  }).success(function(data) {
 			$( section).html( data);
+			$('.mdl-title').tooltip({html: true, placement: "right"}); 
 	})
-}); 
+
+}
