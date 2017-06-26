40a14c5e8882a7bd3341a835f56fb8f48bf28fb1
Max Wuestehube <max.wuestehube@gmail.com>
2017-06-26 16:43:36 +0200

    Fixes file path of JavaScript, introduces comments to JavaScript file getRec.js

--- javascript/getRec.js
+++ javascript/getRec.js
@@ -1,23 +1,32 @@
- 
+// JavaScript for retrieving the recommendation
 
-$( document ).ready(function() {
-var img = document.createElement("img");
-img.src = "img/loading.gif";
-var loading =document.getElementById("loading");
-var br = document.createElement("br");
-loading.appendChild(br);
-loading.appendChild(img);
+$(document).ready(function() {
+	// triggered on page-load
 
-var  id = document.getElementById("gesis_id").innerHTML; 
-var  title = document.getElementById("gesis_title").innerHTML; 
-var encodedtitle = encodeURI(title);
-var etitle = encodedtitle.replace(/'/g, "`");
-var site = "http://sowiport.gesis.org/Dlib/Proxy?id=" + id +"&title=" + etitle;
-var section = document.getElementById("articles");
-    $( section ).load(site);
-    } );   
+	// create loading animation
+	var img = document.createElement("img");
+	img.src = "img/loading.gif";
+	var loading = document.getElementById("loading");
+	var br = document.createElement("br");
+	loading.appendChild(br);
+	loading.appendChild(img);
 
+	// retrieve parameters from HTML
+	var id = document.getElementById("gesis_id").innerHTML; 
+	var title = document.getElementById("gesis_title").innerHTML;
+	// transform title before sending it to the proxy server
+	var encodedtitle = encodeURI(title);
+	var etitle = encodedtitle.replace(/'/g, "`");
 
+	// proxy address
+	var site = "http://sowiport.gesis.org/Dlib/Proxy?id=" + id +"&title=" + etitle;
+
+	// bind the proxy's return value to the articles section
+	var section = document.getElementById("articles");
+    $(section).load(site);
+});   
+
+// introduce logging functionality
 function makeDliblog(aurl,isdlib)
 {
     if(isdlib=='1'){
