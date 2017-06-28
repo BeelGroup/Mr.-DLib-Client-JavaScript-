2fc61330af53a8191366a614e96ad0596fec1354
Sara Yousif <leen_w2@hotmail.com>
2017-06-28 20:23:59 +0400

    Update dlib.js

--- javascript/dlib.js
+++ javascript/dlib.js
@@ -5,14 +5,14 @@ $( window ).ready(function () {
 function get_rec(){
 	 var img = document.createElement("img");
     	img.src = "../../../interface/themes/default_gesis/images/icons/loading.gif";
-   	var loading = document.getElementById("loading");
+   	 var loading = document.getElementById("list");
   	loading.appendChild(img);
  	var id = document.getElementById("gesis_id").innerHTML;
 	var  doc_title = document.getElementById("doc_title_full").innerHTML;
 	var encodedtitle = encodeURI(doc_title);
 	var title = encodedtitle.replace(/'/g, "`");
     	var site = "/Dlib/Proxy?id=" + id +"&title=" + title;
-    	var section = document.getElementById("articles");
+    	var section = document.getElementById("list");
     //$(section).load(site);
 	$.ajax({
 		  type: "GET",
@@ -21,7 +21,12 @@ function get_rec(){
 		  timeout: 3000, // sets timeout to 3 seconds;
 		  }).success(function(data) {
 			$( section).html( data);
-			$('.mdl-title').tooltip({html: true, placement: "right"}); 
-	})
+			 $('.abstract-tooltip').tooltip({html: true, placement: "right"}); 
+			 $('.referesh').click( function(e) {
+                                e.preventDefault(); 
+                                var loading = document.getElementById("list");
+                                loading.innerHTML= "";
+                                get_rec(); return false; } );
+		})
 
 }
